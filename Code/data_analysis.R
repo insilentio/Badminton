709ef9ba3fdb186f00ec2e6d6b733107d1a7e2b0
insilentio <insilentio@protonmail.ch>
2022-01-05 21:18:17 +0100

    figures and plots completed tile figures waterfall and membership chart

--- Code/data_analysis.R
+++ Code/data_analysis.R
@@ -1,6 +1,10 @@
 library(waterfalls)
 
-##some values
+source("data_import.R")
+
+
+# prep some values ----------------------------------------------------------------------------
+
 #all visits
 visits <- stats %>% tally(presence)
 visits$n
@@ -33,6 +37,38 @@ nr_trainings <- stats %>%
   distinct(year, week) %>% 
   count()
 nr_trainings
+#years of active memberships since beginning
+mitgliedschaftsjahre <- stamm %>%
+  count(status) %>%
+  filter(status == "a")
+mitgliedschaftsjahre
+#ratio of female memberships since beginning
+females <- stamm %>%
+  distinct(ID, .keep_all = TRUE) %>%
+  count(sex) %>%
+  mutate(ratio = n/sum(n)) %>%
+  filter(sex == "f")
+females
+#number of distinct members since beginning
+nmembers <- stamm %>%
+  distinct(ID) %>%
+  tally()
+
+
+
+# big figures for tiles in Teilnehmerstatistik ------------------------------------------------
+
+# collect the figures for the Teilnehmerstatistik
+figs <- tibble(cat = "nr of trainings", value = nr_trainings$n) %>%
+  add_row(cat = "all visits", value = visits$n) %>%
+  add_row(cat = "max people in training", value = max(maxmin$max)) %>%
+  add_row(cat = "min people in training", value = min(maxmin$min)) %>%
+  add_row(cat = "years of active memberships", value = mitgliedschaftsjahre$n) %>%
+  add_row(cat = "ratio of female memberships", value = females$ratio) %>%
+  add_row(cat = "number of members", value = nmembers$n)
+
+
+# plot section --------------------------------------------------------------------------------
 
 #overall mean attendance plot
 stats %>%
@@ -41,19 +77,27 @@ stats %>%
   group_by(week) %>%
   summarise(mean=mean(sum)) %>%
   ggplot() +
-  aes(x=week, y=mean) +
-  geom_line() +
-  xlim(c(1,52)) +
-  scale_y_continuous(breaks = c(0:12), limits = c(0,12))
-
-# generate the figures for the Teilnehmerstatistik
-figs <- tibble(cat = "nr of trainings", value = nr_trainings$n) %>%
-  add_row(cat = "all visits", value = visits$n) %>%
-  add_row(cat = "max people in training", value = max(maxmin$max)) %>%
-  add_row(cat = "min people in training", value = min(maxmin$min))
+    aes(x=week, y=mean) +
+    geom_line() +
+    xlim(c(1,52)) +
+    scale_y_continuous(breaks = c(0:12), limits = c(0,12))
 
 #generate the plots for the Teilnehmerstatistik
-wf <- waterfall(cumvisits, draw_lines = FALSE, rect_border = NA) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+wf <- actives %>%
+  left_join(cumvisits, by = "ID", keep = TRUE) %>%
+  rename(ID = ID.x) %>%
+  select(ID, n) %>%
+  waterfall(draw_lines = FALSE, rect_border = NA) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    ggtitle("Kumulierte Teilnahmen der Aktivmitglieder seit 2004")
 wf
 
+mj <- ggplot(actives) +
+  aes(x = ID, y = n_years) +
+  geom_col(fill = "lightblue") +
+  geom_text(aes(x = ID, y = max(n_years), label = since),
+            hjust = 1, angle = 90, colour= "darkgrey") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+        axis.title = element_blank()) +
+  ggtitle("Mitgliedschaftsdauer in Jahren (inkl. Beitrittsjahr)")
+mj
