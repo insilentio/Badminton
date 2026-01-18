2df915df8a5ced942bef683c80a31043da374c5e
insilentio <insilentio@protonmail.ch>
2026-01-18 14:06:45 +0100

    new pipe

--- Code/data_analysis.R
+++ Code/data_analysis.R
@@ -2,12 +2,12 @@
 # prep some values ----------------------------------------------------------------------------
 
 # if desired, manually verify yearly total with excel file
-stats %>%
-  group_by(year) %>%
+stats |>
+  group_by(year) |>
   summarise(tot = sum(presence))
 
 #all visits
-visits <- stats %>% tally(presence)
+visits <- stats |> tally(presence)
 
 #cumulated visits per person total and only during active member years
 cumvisits <- stats |> 
@@ -20,41 +20,41 @@ cumvisits <- stats |>
   arrange(-n)
 
 # max. and min. visits
-maxmin <- stats %>% 
-  filter(type=="Training") %>% 
-  group_by(year, week) %>% 
-  summarise(presence=sum(presence), .groups = "drop_last") %>%
+maxmin <- stats |> 
+  filter(type=="Training") |> 
+  group_by(year, week) |> 
+  summarise(presence=sum(presence), .groups = "drop_last") |>
   summarise(max=max(presence), min=min(presence))
 
 #nr of trainings
-all_trainings <- stats %>% 
-  filter(type=="Training") %>% 
-  distinct(year, week) %>% 
+all_trainings <- stats |> 
+  filter(type=="Training") |> 
+  distinct(year, week) |> 
   count()
 
 #years of active memberships since beginning
-mitgliedschaftsjahre <- stamm %>%
-  count(status) %>%
+mitgliedschaftsjahre <- stamm |>
+  count(status) |>
   filter(status == "a")
 
 #ratio of female memberships since beginning
-females <- stamm %>%
-  distinct(ID, .keep_all = TRUE) %>%
-  count(sex) %>%
-  mutate(ratio = n/sum(n)) %>%
+females <- stamm |>
+  distinct(ID, .keep_all = TRUE) |>
+  count(sex) |>
+  mutate(ratio = n/sum(n)) |>
   filter(sex == "f")
 
 #number of distinct active members since beginning
-nmembers <- stamm %>%
-  filter(n_years > 0) %>%
-  distinct(ID) %>%
+nmembers <- stamm |>
+  filter(n_years > 0) |>
+  distinct(ID) |>
   tally()
 
 # number of trainings
-nr_trainings <- stats %>%
-  group_by(year) %>%
-  filter(type == "Training") %>%
-  distinct(week) %>%
+nr_trainings <- stats |>
+  group_by(year) |>
+  filter(type == "Training") |>
+  distinct(week) |>
   tally()
 
 #determine first and last year in data
@@ -62,11 +62,11 @@ minyear <- min(stats$year)
 maxyear <- max(stats$year)
 
 #only currently active members
-actives <- stamm %>%
-  filter(year == maxyear & status == "a") %>%
-  mutate(Vorname = paste0(Vorname, " ", substr(Nachname, 1, 1), ".")) %>%
-  select(ID, sex, status, n_years, Vorname, since) %>%
-  arrange(desc(n_years)) %>%
+actives <- stamm |>
+  filter(year == maxyear & status == "a") |>
+  mutate(Vorname = paste0(Vorname, " ", substr(Nachname, 1, 1), ".")) |>
+  select(ID, sex, status, n_years, Vorname, since) |>
+  arrange(desc(n_years)) |>
   mutate(ID = factor(ID, levels = ID), Vorname = factor(Vorname, levels = Vorname)) |> 
   left_join(stamm |> 
     filter(year >= 2004 & status == "a") |> 
@@ -79,67 +79,67 @@ maxact <- (max(actives$n_years) %/% 10 +1) * 10
 
 kpi_desc <- c("Besuche/Training", "Standardabweichung", "Besuche (total)", "Besuche\n(Veränderung zum Vorjahr)", "Trainings")
 #KPIs last year
-kpi <- stats %>%
-  filter(year == maxyear & type == "Training") %>%
-  group_by(week) %>%
-  summarise(visits = sum(presence)) %>%
+kpi <- stats |>
+  filter(year == maxyear & type == "Training") |>
+  group_by(week) |>
+  summarise(visits = sum(presence)) |>
   summarise(avg = round(mean(visits), 1),
             sd = round(sd(visits), 1),
-            visits = sum(visits)) %>%
-  bind_cols(upratio = stats %>%
-              filter(year %in% c((maxyear-1):maxyear) & type == "Training") %>%
-              group_by(year) %>%
-              summarise(visits = sum(presence)) %>%
-              mutate(up = 100/accumulate(visits, `/`)-100) %>%
-              pluck(3,2) %>%
-              round(0)) %>%
-  bind_cols(nr_tr = (nr_trainings %>% filter(year == maxyear))$n) %>%
-  pivot_longer(everything(), names_to = "cat", values_to = "values") %>%
-  mutate(desc = kpi_desc, idx = seq(9,1,-2)) %>%
-  mutate(values = as.character(values)) %>%
+            visits = sum(visits)) |>
+  bind_cols(upratio = stats |>
+              filter(year %in% c((maxyear-1):maxyear) & type == "Training") |>
+              group_by(year) |>
+              summarise(visits = sum(presence)) |>
+              mutate(up = 100/accumulate(visits, `/`)-100) |>
+              pluck(3,2) |>
+              round(0)) |>
+  bind_cols(nr_tr = (nr_trainings |> filter(year == maxyear))$n) |>
+  pivot_longer(everything(), names_to = "cat", values_to = "values") |>
+  mutate(desc = kpi_desc, idx = seq(9,1,-2)) |>
+  mutate(values = as.character(values)) |>
   mutate(values = if_else(cat == "upratio",
                           if_else(values >= 0, paste0("+", values, "%"), paste0("-", values, "%")),
                                   values))
 
 
 #med, mean, max, min visits per person
-summaries <- stats %>%
-  inner_join(actives, by = "ID", keep = TRUE) %>%
-  group_by(ID.y, year) %>%
-  summarise(visits = sum(presence), .groups = "drop_last") %>%
+summaries <- stats |>
+  inner_join(actives, by = "ID", keep = TRUE) |>
+  group_by(ID.y, year) |>
+  summarise(visits = sum(presence), .groups = "drop_last") |>
   summarise(med = median(visits),
             avg = mean(visits),
             max = max(visits),
             min = min(visits),
-            .groups = "keep") %>%
+            .groups = "keep") |>
   arrange(ID.y)
 
 #mean attendance per year, per person
-mean_pp <- stats %>%
-  filter(!grepl("Gäste", ID) & !grepl("Passive", ID)) %>% 
-  filter(type=="Training") %>%
-  group_by(ID,year) %>% 
-  summarise(sum=sum(presence), .groups = "drop_last") %>% 
-  summarise(mean=mean(sum), tot=mean*n()) %>%
+mean_pp <- stats |>
+  filter(!grepl("Gäste", ID) & !grepl("Passive", ID)) |> 
+  filter(type=="Training") |>
+  group_by(ID,year) |> 
+  summarise(sum=sum(presence), .groups = "drop_last") |> 
+  summarise(mean=mean(sum), tot=mean*n()) |>
   arrange(-mean)
 
 # big figures for tiles in Teilnehmerstatistik ------------------------------------------------
 
 # collect the figures for the Teilnehmerstatistik
 figs <- tibble(cat = "Trainings", value = all_trainings$n,
-               head = "seit 2004") %>%
+               head = "seit 2004") |>
   add_row(cat = "Trainingsbesuche", value = visits$n,
-          head = "seit 2004") %>%
+          head = "seit 2004") |>
   add_row(cat = "max. Teilnehmerzahl", value = max(maxmin$max),
-          head = "seit 2004") %>%
+          head = "seit 2004") |>
   add_row(cat = "min. Teilnehmerzahl", value = min(maxmin$min),
-          head = "seit 2004") %>%
+          head = "seit 2004") |>
   add_row(cat = "Jahre Aktivmitgliedschaften", value = mitgliedschaftsjahre$n,
-          head = "seit Gründung")  %>%
+          head = "seit Gründung")  |>
   add_row(cat = "bisherige Aktivmitglieder", value = nmembers$n,
-          head = "seit Gründung") %>%
-  mutate(value = as.character(value)) %>%
+          head = "seit Gründung") |>
+  mutate(value = as.character(value)) |>
   add_row(cat = "Frauenanteil", value = label_percent(accuracy = .1)(females$ratio),
-          head = "seit Gründung", .after = 5)%>%
+          head = "seit Gründung", .after = 5) |> 
   mutate(cat = factor(cat, levels = cat))
 
--- Code/data_import.R
+++ Code/data_import.R
@@ -19,40 +19,40 @@ for (i in 1:(length(sheets)-1)) {
   #tidy it up
   #replace all x's with 1, name the sheets with resp. year, name cols, filter lot of rows and
   #finally gather all together
-  single <- single %>% 
-          replace(. == "x", "1") %>%
-          replace(. == "Total Trainings", "Trainings") %>%
-          mutate(year=sheets[i]) %>%
-          select("ID", "year", all_of(events)) %>%
-          mutate(across(1, as.character)) %>%
-          mutate(across(c(2:55), as.numeric)) %>%
-          filter(!is.na(ID)) %>%
-          pivot_longer(all_of(events), names_to="week", values_to = "presence") %>%
+  single <- single |> 
+          replace(. == "x", "1") |>
+          replace(. == "Total Trainings", "Trainings") |>
+          mutate(year=sheets[i]) |>
+          select("ID", "year", all_of(events)) |>
+          mutate(across(1, as.character)) |>
+          mutate(across(c(2:55), as.numeric)) |>
+          filter(!is.na(ID)) |>
+          pivot_longer(all_of(events), names_to="week", values_to = "presence") |>
           mutate(across("presence", ~if_else(is.na(presence),0,presence)))
   
   #let's evaluate if respective week was vacation or not
   #(if at least somebody was around, then obviously not)
-  trainings <- single %>%
-    pivot_wider(names_from ="ID", values_from = "presence") %>%
-    pivot_longer(-c("year", "week", "Trainings"), names_to = "ID", values_to = "presence") %>%
-    group_by(week, Trainings) %>%
-    summarise(sum = sum(presence), .groups = "drop") %>%
-    mutate(type = if_else(sum > 0, "Training", "Ferien"))  %>%
+  trainings <- single |>
+    pivot_wider(names_from ="ID", values_from = "presence") |>
+    pivot_longer(-c("year", "week", "Trainings"), names_to = "ID", values_to = "presence") |>
+    group_by(week, Trainings) |>
+    summarise(sum = sum(presence), .groups = "drop") |>
+    mutate(type = if_else(sum > 0, "Training", "Ferien"))  |>
     mutate(type = if_else(week == "99", "Turnier", type))
   
   # move to next iteration if no trainings available (e.g. current year)
-  if ((trainings %>% summarise(sum = sum(sum)))$sum == 0) next
+  if ((trainings |> summarise(sum = sum(sum)))$sum == 0) next
   
   #combine the 2 datasets. Col. "Trainings" can be dropped later, but is used for imputation in next step
   #some entries have to be filtered out as they contain aggregated values already (e.g. "Aktive")
-  single <- left_join(single, trainings, by="week") %>%
-    select("ID", "year", "week", "presence", "type", "Trainings") %>%
-    mutate_at(vars("week"), "as.numeric") %>%
+  single <- left_join(single, trainings, by="week") |>
+    select("ID", "year", "week", "presence", "type", "Trainings") |>
+    mutate_at(vars("week"), "as.numeric") |>
     filter(ID!="Aktive" & ID!="Total" & ID!="Mittelwert" & ID!="Trainings" & ID!="Passive" & ID!="Gäste")
   
   #split Turnier visits into separate variable; otherwise tends to be forgotten later
-  turnier_single <- single %>% filter(type=="Turnier")
-  single <- single %>% filter(type!="Turnier")
+  turnier_single <- single |> filter(type=="Turnier")
+  single <- single |> filter(type!="Turnier")
   
   #now the part with the imputation:
   #years <2013 are incomplete regarding presence stats due to absences of Irene. Only trainings
@@ -61,29 +61,29 @@ for (i in 1:(length(sheets)-1)) {
   #Such values are imputed by simply interpolating:
   #(nr. of trainings w/stats) divided by (nr. of training weeks total) times (trainings attended)
   #values are then assigned randomly to missing weeks
-  n_train <- single %>%
-    group_by(type, week) %>%
-    summarise(n=n_distinct(week), .groups = "drop_last") %>%
-    summarise(n=sum(n), .groups = "drop") %>%
+  n_train <- single |>
+    group_by(type, week) |>
+    summarise(n=n_distinct(week), .groups = "drop_last") |>
+    summarise(n=sum(n), .groups = "drop") |>
     filter(type=="Training")
-  data_train <- single %>%
-    group_by(Trainings, week) %>% 
-    summarise(n=n_distinct(week), .groups = "drop_last") %>%
-    summarise(n=sum(n), .groups = "drop") %>%
+  data_train <- single |>
+    group_by(Trainings, week) |> 
+    summarise(n=n_distinct(week), .groups = "drop_last") |>
+    summarise(n=sum(n), .groups = "drop") |>
     filter(Trainings==1)
   
   quote <- n_train$n / data_train$n
   if (quote < 1) {
     #then we have a mismatch between effective trainings and trainings with data
     #first we define which weeks are missing
-    mw <- single %>% 
-      filter(Trainings == 1 & type == "Ferien") %>% 
+    mw <- single |> 
+      filter(Trainings == 1 & type == "Ferien") |> 
       distinct(week)
     #then how many attendances per person have to be added (except Irene)
-    ma <- single %>%
-      filter(ID != "SchwarzenbachIrene") %>%
-      group_by(ID) %>% 
-      summarise(sum = sum(presence), .groups = "drop") %>% 
+    ma <- single |>
+      filter(ID != "SchwarzenbachIrene") |>
+      group_by(ID) |> 
+      summarise(sum = sum(presence), .groups = "drop") |> 
       mutate(interpoliert = round(sum/quote) - sum)
     #now select randomly for each ID as many of the missing weeks as additional attendances were determined
     for (j in 1:nrow(ma)) {
@@ -96,7 +96,7 @@ for (i in 1:(length(sheets)-1)) {
         } else {
           fill_ins <- mw$week * ma[j,]$interpoliert
         }
-        single <- single %>% 
+        single <- single |> 
           mutate(presence = if_else(ID == ma[j,]$ID & week %in% fill_ins,1,presence))
       }
       #now separately impute for Gäste and Passive
@@ -104,9 +104,9 @@ for (i in 1:(length(sheets)-1)) {
         #first we define on how many trainings we want to split the interpolated values.
         #this is determined by counting all trainings with at least 1 attendance of
         #Gäste/Passive (similar to attendance of a person)
-        miss_gp <- single %>%
-          filter(ID == ma[j,]$ID, type=="Training", presence>0) %>%
-          group_by(ID) %>% tally() %>% 
+        miss_gp <- single |>
+          filter(ID == ma[j,]$ID, type=="Training", presence>0) |>
+          group_by(ID) |> tally() |> 
           mutate(interpoliert = round(n/quote) - n)
         
         if (length(mw$week) > 1) {
@@ -124,10 +124,10 @@ for (i in 1:(length(sheets)-1)) {
           trs[k] <- trs[k] + 1
         }
 
-        single <- single %>%
-          filter(ID == ma[j,]$ID & week %in% fill_ins) %>%
-          mutate(presence = trs) %>%
-          union(single %>% filter(!(ID == ma[j,]$ID & week %in% fill_ins)))
+        single <- single |>
+          filter(ID == ma[j,]$ID & week %in% fill_ins) |>
+          mutate(presence = trs) |>
+          union(single |> filter(!(ID == ma[j,]$ID & week %in% fill_ins)))
       }
     }
   }
@@ -158,11 +158,11 @@ names(stamm) <- cn2
 
 
 #all members
-stamm <- stamm %>%
-  slice(-1) %>%
-  pivot_longer(starts_with("y"), names_to = "year",  values_to = "status") %>%
-  select(ID, sex, year, status, JahreaktivimVerein, Mitgliedseit, Nachname, Vorname) %>%
-  mutate(year = substr(year, 2, 5)) %>%
-  rename(n_years = JahreaktivimVerein, since = Mitgliedseit) %>%
-  mutate(across(c("year", "n_years"), as.numeric)) %>%
+stamm <- stamm |>
+  slice(-1) |>
+  pivot_longer(starts_with("y"), names_to = "year",  values_to = "status") |>
+  select(ID, sex, year, status, JahreaktivimVerein, Mitgliedseit, Nachname, Vorname) |>
+  mutate(year = substr(year, 2, 5)) |>
+  rename(n_years = JahreaktivimVerein, since = Mitgliedseit) |>
+  mutate(across(c("year", "n_years"), as.numeric)) |>
   filter(!is.na(ID))
